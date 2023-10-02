e0879a7d808fba5c7c92005f2cd3fd064ad1bb37
Pratik Saxena <pratik2709@gmail.com>
2023-10-02 19:55:32 +0530

    refactor

---
+++ part_2.py
@@ -0,0 +1,56 @@
+class BaseCaseOperations:
+    def __init__(self):
+        pass
+
+    def convert_to(self, input_string):
+        pass
+
+    def is_valid(self, input_string):
+        pass
+
+
+# camelcase
+class CamelCase(BaseCaseOperations):
+    def __init__(self):
+        super().__init__()
+
+    def convert_to(self, input_string):
+        pass
+
+    def is_valid(self, input_string):
+        pass
+
+
+class PascalCase(BaseCaseOperations):
+    def __init__(self):
+        super().__init__()
+
+    def convert_to(self, input_string):
+        pass
+
+    def is_valid(self, input_string):
+        pass
+
+
+    # snake_case
+class SnakeCase(BaseCaseOperations):
+    def __init__(self):
+        super().__init__()
+
+    def convert_to(self, input_string):
+        pass
+
+    def is_valid(self, input_string):
+        pass
+
+
+# kebab-case
+class KebabCase(BaseCaseOperations):
+    def __init__(self):
+        super().__init__()
+
+    def convert_to(self, input_string):
+        pass
+
+    def is_valid(self, input_string):
+        pass
